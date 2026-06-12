81b886153282208a70b52166ccb168a38162056c
jsofioa <josefinasofio@gmail.com>
2026-06-12 06:44:32 +0000

    Updates

--- src/components/ButtonBack/ButtonBack.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-import "./ButtonBack.css";
-
-export const ButtonBack = () => {
-    return(<section className="btn-back-section">
-        <Link to={"/"}><button className="btn-back">← Volver</button></Link>
-    </section>
-    );
-};
--- src/components/ItemDetail/ItemDetail.jsx
+++
@@ -1,9 +0,0 @@
-import { ItemFull } from "../Item/Item";
-import "./ItemDetail.css";
-
-export const ItemDetail = ({item}) =>{
-    return (<div className="product-container">
-        <ItemFull {...item} />
-        </div>
-    )
-};
